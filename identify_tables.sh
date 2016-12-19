5d38d80a60f33541b10a9b260a45ef48b8818fe1
george-cap <george.cj@capillarytech.com>
2016-12-19 12:18:45 +0530

    minor changes

--- identify_tables.sh
+++ identify_tables.sh
@@ -2,11 +2,14 @@
 password=root
 username=root
 
+dump_folder=dumps
+org_id=1447
+mkdir -p $dump_folder/data
 mysql -p$password -u $username -e "show databases" > db_name.txt
 
 for db_name in `cat db_name.txt`; do 
-	mysql -p$password -u $username -e "select table_schema, table_name, column_name from information_schema.columns where column_name in ('org_id', 'publisher_id') and table_name not in ('Temp', 'tempdb', 'data_cleanup') and TABLE_SCHEMA = '$db_name'" > tables.txt;
+	mysql -p$password -u $username -e "select table_schema, table_name, column_name from information_schema.columns where column_name in ('org_id', 'publisher_id') and table_name not in ('Temp', 'tempdb', 'data_cleanup', 'performance_logs') and TABLE_SCHEMA = '$db_name'" > $dump_folder/tables.txt;
 	for tbl_name in `cat tables.txt`; do
-		echo "mysqldump -p $password -u $username $dbname $tbl_name --no-create-info --compact --skip-comments --skip-utc --skip-lock-tables --where 'org_id=23' " >>"$db_name"_dump.sh
+		echo "mysqldump -p $password -u $username $dbname $tbl_name --no-create-info --compact --skip-comments --skip-utc --skip-lock-tables --where 'org_id=1447' " >> $dump_folder/data/"$db_name"_dump.sh
 	done;
 done;
